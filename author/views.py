05eaee12a28a090a6c6f0ff3a9160521a461cbef
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-09-15 21:28:00 -0300

    feat: Criar o formulário de Login do Author
    Para criar o login do autor foi primeiro feito o formulário com os campos username e password e em seguida na views do autor a lógica para efetuar o login.

--- author/views.py
+++ author/views.py
@@ -1,10 +1,22 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, session
 from flask_blog import app
-from author.form import RegisterForm
+from author.form import RegisterForm, LoginForm
+from author.models import Author
 
-@app.route('/login')
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-    return 'Author login'
+    form = LoginForm()
+    error = None
+
+    if form.validate_on_submit():
+        author = Author.query.filter_by(
+            username=form.username.data,
+            password=form.password.data
+            ).limit(1)
+        if author.count():
+            session['username'] = form.username.data
+            return redirect(url_for('login_success'))
+    return render_template('author/login.html', form=form, error=error)
 
 @app.route('/register', methods=('GET', 'POST'))
 def register():
@@ -16,3 +28,7 @@ def register():
 @app.route('/success')
 def success():
     return "Author registered!"
+
+@app.route('/login_success')
+def login_success():
+    return "Author logged in!"
