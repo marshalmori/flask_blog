247757c2b68df340ee0ab130f169d76ad9beab44
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-09-18 22:32:46 -0300

    feat: Criar o formulário com as informações para o post
    Foi criado o formulário para criar o post e a tabela post e category no banco de dados para armazenar essas informações.

--- blog/form.py
+++ blog/form.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField
+from wtforms import validators, StringField, TextAreaField
 from author.form import RegisterForm
+from blog.models import Category
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 # Como a classe SetupForm herda a classe RegisterForm, ela tem todas
 # as características de RegisterForm
@@ -9,3 +11,15 @@ class SetupForm(RegisterForm):
     validators.Required(),
     validators.Length(max=80)
     ])
+
+def categories():
+    return Category.query
+
+class PostForm(FlaskForm):
+    title = StringField('Title', [
+        validators.Required(),
+        validators.Length(max=80)
+        ])
+    body = TextAreaField('Content', validators=[validators.Required()])
+    category = QuerySelectField('Category', query_factory=categories, allow_blank=True)
+    new_category = StringField('New Category')
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from flask_blog import app
 from flask import render_template, redirect, flash, url_for, session, abort
-from blog.form import SetupForm
+from blog.form import SetupForm, PostForm
 from flask_blog import db
 from author.models import Author
 from blog.models import Blog
-from author.decorators import login_required
+from author.decorators import login_required, author_required
 import bcrypt
 
 @app.route('/')
@@ -16,7 +16,7 @@ def index():
     return render_template('blog/index.html')
 
 @app.route('/admin')
-@login_required
+@author_required
 def admin():
     if session.get('is_author'):
         return render_template('blog/admin.html')
@@ -57,3 +57,13 @@ def setup():
             db.session.rollback()
             error = "Error creating blog"
     return render_template('blog/setup.html', form=form, error = error)
+
+@app.route('/post', methods=('GET', 'POST'))
+@author_required
+def post():
+    form = PostForm()
+    return render_template('blog/post.html', form=form)
+
+@app.route('/article')
+def article():
+    return render_template('blog/article.html')
