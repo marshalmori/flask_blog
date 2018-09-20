25e1045258dd796a6aafaa4981c7854ef334620b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-09-20 10:35:32 -0300

    feat: Criar estrutura de pastas e configurações no banco de dados para upload de imagens.
    Foi criado a pasta images dentro da pasta static e foi configurado os imports no arquivo __init__ e inserido os campos no arquivo models do blog para gerar a coluna image.

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class Post(db.Model):
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
     title = db.Column(db.String(80))
     body = db.Column(db.Text)
+    image = db.Column(db.String(255))
     slug = db.Column(db.String(250), unique=True)
     publish_date = db.Column(db.DateTime)
     live = db.Column(db.Boolean)
@@ -27,6 +28,10 @@ class Post(db.Model):
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship('Category',backref=db.backref('posts', lazy='dynamic'))
 
+    @property
+    def imgsrc(self):
+        return uploaded_images.url(self.image)
+        
     def __init__(self, blog, author, title, body, category, slug=None, publish_date=None, live=True):
         self.blog_id = blog.id
         self.author_id = author.id
